e48f659616328cf6106628c0a21621e87bfec74b
hieunv-0998 <nguyen.van.hieu@sun-asterisk.com>
2024-07-25 01:20:24 +0700

    fix: Type error: Type 'string' is not assignable to type 'boolean'.

--- frontend/src/app/[lang]/components/Aioa.tsx
+++ frontend/src/app/[lang]/components/Aioa.tsx
@@ -10,7 +10,7 @@ export default function Aioa() {
     const aioa_script_tag = document.createElement("script");
     aioa_script_tag.src = "https://www.skynettechnologies.com/accessibility/js/all-in-one-accessibility-js-widget-minify.js?colorcode=#420083&token=ADAAIOA-7AF984B7E8846DEFF6B744A52D4D3315&position=bottom_right";
     aioa_script_tag.id = "aioa-adawidget";
-    aioa_script_tag.defer = "true";
+    aioa_script_tag.defer = true;
     document.body.appendChild(aioa_script_tag);
   }, []);
 
